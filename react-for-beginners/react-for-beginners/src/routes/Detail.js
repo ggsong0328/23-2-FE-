32c57abac03ab87bfe2f81eb4d0f0c9d0baddab5
ggsong0328 <ggsong0328@gmail.com>
2024-01-26 20:20:54 +0900

    7.6 - Parameters

--- react-for-beginners/react-for-beginners/src/routes/Detail.js
+++ react-for-beginners/react-for-beginners/src/routes/Detail.js
@@ -1,5 +1,37 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import Movie from "../components/Movie";
+
 function Detail() {
-  return <h1>Detail</h1>;
+  const [loading, setLoading] = useState(true);
+  const [details, setDetails] = useState([]);
+  const { id } = useParams();
+  const getMovie = async () => {
+    const json = await (
+      await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
+    ).json();
+    setDetails(json.data.movie);
+    setLoading(false);
+  };
+  useEffect(() => {
+    getMovie();
+  }, []);
+  console.log(details);
+  return (
+    <div>
+      {loading ? (
+        <h1>Loading</h1>
+      ) : (
+        <Movie
+          id={details.id}
+          coverImg={details.medium_cover_image}
+          title={details.title}
+          summary={details.summary}
+          genres={details.genres}
+        />
+      )}
+    </div>
+  );
 }
 
 export default Detail;
